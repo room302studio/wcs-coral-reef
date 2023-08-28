143a0bf2316377295e7779a8069f6c691ff44abc
EJ Fox <ejfox@ejfox.com>
2023-08-28 11:43:29 -0400

    feat: scaffold out initial site structure according to sketch

--- app.vue
+++ app.vue
@@ -1,7 +1,213 @@
 <template>
-  <div>
-    <!-- <NuxtWelcome class="debug"/> -->
-    <h1>Coral Reefs</h1>
+  <div class="pa5 pr6">
+
+    <div class="vh-50 w-100 pa4">
+      <h1>Intro Title</h1>
+    </div>
+
+    <div id="floating-nav" class="fixed right-1 top-1 bg-white pa2 w-10 f6">
+      <h4 class="ma0 pa0">Navigation</h4>
+      <ul class="pa0 mt2 mb0 list">
+        <li class="mv2">Introduction</li>
+        <li class="mv2">Global temperature rise</li>
+        <li class="mv2">Impact on world and oceans</li>
+        <li class="mv2">Ocean reef percentage</li>
+        <li class="mv2">Reef type breakdown</li>
+        <li class="mv2">Reef type: resist</li>
+        <li class="mv2">Reef type: recover</li>
+        <li class="mv2">Reef type: avoid</li>
+        <li class="mv2">Explore them all</li>
+        <li class="mv2">Call to action</li>
+      </ul>
+    </div>
+
+    <div class="cf">
+      <div class="measure fl">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum similique ratione ex explicabo soluta ab fuga
+        minima, qui nobis omnis doloribus unde magni quod modi quos! Porro necessitatibus eveniet voluptates!
+      </div>
+    </div>
+
+    <div id="surface-of-water">
+      <hr />
+    </div>
+
+    <div id="section-1 cf" class="section">
+      <div class="w-two-thirds fl">
+        <h1>Chart of global temp rise</h1>
+      </div>
+
+      <div class="w-third fl">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae libero provident dolorem excepturi minus
+          quaerat quasi assumenda optio quod eveniet, voluptatum pariatur ducimus facere, consequatur reiciendis ea vero
+          aliquam nam.</p>
+      </div>
+    </div>
+
+    <div id="section-2" class="section cf">
+      <div class="w-50 fl">
+        <h2 class="ma0 pa0">Impact on world</h2>
+        <ul>
+          <li>Impact 1</li>
+          <li>Impact 2</li>
+          <li>Impact 3</li>
+          <li>Impact 4</li>
+
+        </ul>
+      </div>
+
+      <div class="w-50 fl">
+        <h2 class="ma0 pa0">Impact on oceans</h2>
+        <ul>
+          <li>Impact 1</li>
+          <li>Impact 2</li>
+          <li>Impact 3</li>
+          <li>Impact 4</li>
+
+        </ul>
+      </div>
+    </div>
+
+    <div id="section-3" class="section w-100 cf">
+      <div class="w-50 fl ">
+        <h1>Ocean/reef percentage</h1>
+      </div>
+
+      <div class="w-50 fl">
+        TK% of the world's oceans are Coral Reefs
+      </div>
+    </div>
+
+    <div id="section-4" class="section w-100 cf">
+      <h2>Reef type breakdown</h2>
+      <div class="h5 ba b--black bw3 w-100 ">
+
+      </div>
+      <div class="cf">
+        <div class="reef-type w-third fl">
+          Resist
+        </div>
+        <div class="reef-type w-third fl">
+          Recover
+        </div>
+        <div class="reef-type w-third fl">
+          Avoid
+        </div>
+      </div>
+    </div>
+
+    <ReefTypeModule class="ba b--black pa4" :title="'Resist'" />
+
+    <ReefTypeModule class="ba b--black pa4" :title="'Recover'" />
+
+    <ReefTypeModule class="ba b--black pa4" :title="'Avoid'" />
+
+    <div id="section-6" class="section w-100">
+      <h2>Explore them all</h2>
+      <div class="cf">
+        <div class="w-50 h5 ba b--black fl">
+          <!-- use flex to vertically center -->
+          <div class="flex flex-column justify-center items-center h-100">
+            <h3>Spinny globe</h3>
+          </div>
+        </div>
+
+        <div class="w-50 fl ph5">
+          <h2 class="pa0 ma0">Metadata</h2>
+          <ul class="list pa0">
+            <li><strong>Item:</strong> Value </li>
+            <li><strong>Item:</strong> Value </li>
+            <li><strong>Item:</strong> Value </li>
+            <li><strong>Item:</strong> Value </li>
+            <li><strong>Item:</strong> Value </li>
+            <li><strong>Item:</strong> Value </li>
+            <li><strong>Item:</strong> Value </li>
+            <li><strong>Item:</strong> Value </li>
+            <li><strong>Item:</strong> Value </li>
+            <li><strong>Item:</strong> Value </li>
+          </ul>
+        </div>
+
+        <div class="w-100">
+          <h3>Table of reefs</h3>
+          <table class="w-100 f5">
+            <thead>
+              <tr class="tl bg-black white">
+                <th class="pa2">Reef</th>
+                <th class="pa2">Location</th>
+                <th class="pa2">Country</th>
+                <th class="pa2">Region</th>
+                <th class="pa2">Size</th>
+                <th class="pa2">Threat</th>
+              </tr>
+            </thead>
+            <tbody>
+              <!-- just make 20 example reefs -->
+              <tr v-for="i in 20" :key="i" :style="{
+                backgroundColor: i === 7 ? '#CCC' : ''
+              }">
+                <td>Reef {{ i }}</td>
+                <td>Location {{ i }}</td>
+                <td>Country {{ i }}</td>
+                <td>Region {{ i }}</td>
+                <td>Size {{ i }}</td>
+                <td>Threat {{ i }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div id="section-7" class="section">
+        <h2>Call to action</h2>
+        <div class="measure-wide">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos saepe ducimus natus accusamus beatae error
+          reprehenderit corrupti! Nulla beatae, sed perferendis nisi labore perspiciatis. Suscipit sit maiores placeat
+          error libero.
+        </div>
+
+        <!-- email sign up form -->
+        <div>
+          <form action="">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" />
+            <button type="submit">Sign up</button>
+          </form>
+        </div>
+
+        <div class="w-100 cf">
+          <div class="w-third fl">
+            <h2 class="f1">Gov</h2>
+            <div>arrow</div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sequi iusto at perferendis quaerat
+              explicabo ut. Quod ducimus doloremque odio, et maxime officia odit, magnam ea pariatur debitis deserunt
+              commodi?</div>
+            <button>CTA</button>
+          </div>
+          <div class="w-third fl">
+            <h2 class="f1">Funders</h2>
+            <div>arrow</div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sequi iusto at perferendis quaerat
+              explicabo ut. Quod ducimus doloremque odio, et maxime officia odit, magnam ea pariatur debitis deserunt
+              commodi?</div>
+            <button>CTA</button>
+          </div>
+          <div class="w-third fl">
+            <h2 class="f1">Scientists</h2>
+            <div>arrow</div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sequi iusto at perferendis quaerat
+              explicabo ut. Quod ducimus doloremque odio, et maxime officia odit, magnam ea pariatur debitis deserunt
+              commodi?</div>
+            <button>CTA</button>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
+
+
   </div>
 </template>
 <script setup>
@@ -12,7 +218,7 @@ const { $toast } = useNuxtApp()
 onMounted(() => {
   enableToast()
 
-  let hello = $toast.success('Hello World')
+  // let hello = $toast.success('Hello World')
 
   // force dismiss hello
   // hello.dismiss()
@@ -22,86 +228,12 @@ onMounted(() => {
 /* use Figtree font for everything */
 body {
   font-family: 'Figtree', sans-serif;
+  font-size: 24px;
 }
 
-.v-toast__text {
-  font-family: 'Figtree', sans-serif;
-  font-size: 0.75em;
+.section {
+  min-height: 66vh;
+  margin-top: 22vh;
+  margin-bottom: 22vh;
 }
-
-/* override the default toast styles */
-.v-toast__item {
-  padding: 0.25em;
-  box-shadow: 0 0 10px 0 rgba(0,0,0,0.15);
-}
-
-.v-toast__item--success {
-  /* background-color: #4caf50; */
-  /* grayscale */
-  background-color: #222;
-  /* animation: bgrainbow 15s infinite; */
-    
-  /* make a border that animates through the rainbow */
-  border: 1px solid #000;
-  animation: rainbow 22s infinite;
-}
-
-.v-toast__item--error {
-  background-color: #f44336;
-  /* grayscale */
-  /* background-color: #999; */
-}
-
-.v-toast__item--warning {
-  /* background-color: #ffc107; */
-  /* grayscale */
-  background-color: #666;
-  color: #000;
-  animation: rainbow 12s infinite;
-}
-
-.v-toast__item--info {
-  /* background-color: #2196f3; */
-  /* grayscale */
-  background-color: #CCC;
-  color: #000;
-}
-
-
-@keyframes rainbow {
-  0% {
-    border-color: #f00;
-  }
-  10% {
-    border-color: #ff0;
-  }
-  20% {
-    border-color: #0f0;
-  }
-  30% {
-    border-color: #0ff;
-  }
-  40% {
-    border-color: #00f;
-  }
-  50% {
-    border-color: #f0f;
-  }
-  60% {
-    border-color: #f00;
-  }
-  70% {
-    border-color: #ff0;
-  }
-  80% {
-    border-color: #0f0;
-  }
-  90% {
-    border-color: #0ff;
-  }
-  100% {
-    border-color: #00f;
-  }
-}
-
 </style>
---
+++ components/ReefTypeModule.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="section-5" class="section w-100 cf">
+    <h1>{{title}}</h1>
+    <div class="cf">
+      <div class="w-50 fl">
+        How they resist
+      </div>
+
+      <div class="w-50 fl">
+        <h2>Intro graf</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum tempore quae repudiandae voluptate, impedit
+          possimus magnam eos voluptas at, natus saepe blanditiis vero incidunt eum recusandae officia, expedita
+          consequuntur molestias?</p>
+      </div>
+    </div>
+
+    <div>
+      <h2>Where</h2>
+
+      <!-- make a 2x3 grid of map locations -->
+      <div class="cf">
+        <div class="w-third fl">
+          <h3>Location 1</h3>
+          <img class="w-100 h5" style="background-color: #CCC;" />
+          <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p> -->
+        </div>
+        <div class="w-third fl">
+          <h3>Location 2</h3>
+          <img class="w-100 h5" style="background-color: #CCC;" />
+          <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p> -->
+        </div>
+        <div class="w-third fl">
+          <h3>Location 3</h3>
+          <img class="w-100 h5" style="background-color: #CCC;" />
+          <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p> -->
+        </div>
+        <div class="w-third fl">
+          <h3>Location 4</h3>
+          <img class="w-100 h5" style="background-color: #CCC;" />
+          <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p> -->
+        </div>
+        <div class="w-third fl">
+          <h3>Location 5</h3>
+          <img class="w-100 h5" style="background-color: #CCC;" />
+          <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p> -->
+        </div>
+        <div class="w-third fl">
+          <h3>Location 6</h3>
+          <img class="w-100 h5" style="background-color: #CCC;" />
+          <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p> -->
+        </div>
+      </div>
+
+      <div>
+        <h2>Case Study</h2>
+        <div class="cf">
+          <div class="w-50 fl">
+            Satellite photo
+          </div>
+
+          <div class="w-50 fl">
+            Ocean photo slideshow
+          </div>
+        </div>
+
+        <div class="pa5 w-100">
+          What makes it special
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+<script setup>
+const { title } = defineProps({
+  title: String,
+})
+
+</script>

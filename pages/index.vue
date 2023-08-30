b7d2a478c29fd2bc0cb41f9238c83a6b814f1e50
Curran <curran.kelleher@gmail.com>
2023-08-30 14:44:27 -0400

    Try felt embed

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="pa5 pr6">
-
     <div class="vh-50 w-100 pa4">
       <h1>Intro Title</h1>
     </div>
@@ -23,8 +22,10 @@
 
     <div class="cf">
       <div class="measure fl">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum similique ratione ex explicabo soluta ab fuga
-        minima, qui nobis omnis doloribus unde magni quod modi quos! Porro necessitatibus eveniet voluptates!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum
+        similique ratione ex explicabo soluta ab fuga minima, qui nobis omnis
+        doloribus unde magni quod modi quos! Porro necessitatibus eveniet
+        voluptates!
       </div>
     </div>
 
@@ -38,9 +39,12 @@
       </div>
 
       <div class="w-third fl">
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae libero provident dolorem excepturi minus
-          quaerat quasi assumenda optio quod eveniet, voluptatum pariatur ducimus facere, consequatur reiciendis ea vero
-          aliquam nam.</p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          libero provident dolorem excepturi minus quaerat quasi assumenda optio
+          quod eveniet, voluptatum pariatur ducimus facere, consequatur
+          reiciendis ea vero aliquam nam.
+        </p>
       </div>
     </div>
 
@@ -52,7 +56,6 @@
           <li>Impact 2</li>
           <li>Impact 3</li>
           <li>Impact 4</li>
-
         </ul>
       </div>
 
@@ -63,36 +66,25 @@
           <li>Impact 2</li>
           <li>Impact 3</li>
           <li>Impact 4</li>
-
         </ul>
       </div>
     </div>
 
     <div id="section-3" class="section w-100 cf">
-      <div class="w-50 fl ">
+      <div class="w-50 fl">
         <h1>Ocean/reef percentage</h1>
       </div>
 
-      <div class="w-50 fl">
-        TK% of the world's oceans are Coral Reefs
-      </div>
+      <div class="w-50 fl">TK% of the world's oceans are Coral Reefs</div>
     </div>
 
     <div id="section-4" class="section w-100 cf">
       <h2>Reef type breakdown</h2>
-      <div class="h5 ba b--black bw3 w-100 ">
-
-      </div>
+      <div class="h5 ba b--black bw3 w-100"></div>
       <div class="cf">
-        <div class="reef-type w-third fl">
-          Resist
-        </div>
-        <div class="reef-type w-third fl">
-          Recover
-        </div>
-        <div class="reef-type w-third fl">
-          Avoid
-        </div>
+        <div class="reef-type w-third fl">Resist</div>
+        <div class="reef-type w-third fl">Recover</div>
+        <div class="reef-type w-third fl">Avoid</div>
       </div>
     </div>
 
@@ -104,6 +96,13 @@
 
     <div id="section-6" class="section w-100">
       <h2>Explore them all</h2>
+      <iframe
+        width="100%"
+        height="600"
+        frameborder="0"
+        title="Felt Map"
+        src="https://felt.com/embed/map/Coral-Reefs-Y1O4GeEVRVuGF7EJN49AZxC?loc=-1.04,114.89,3z"
+      ></iframe>
       <div class="cf">
         <div class="w-50 h5 ba b--black fl">
           <!-- use flex to vertically center -->
@@ -115,16 +114,16 @@
         <div class="w-50 fl ph5">
           <h2 class="pa0 ma0">Metadata</h2>
           <ul class="list pa0">
-            <li><strong>Item:</strong> Value </li>
-            <li><strong>Item:</strong> Value </li>
-            <li><strong>Item:</strong> Value </li>
-            <li><strong>Item:</strong> Value </li>
-            <li><strong>Item:</strong> Value </li>
-            <li><strong>Item:</strong> Value </li>
-            <li><strong>Item:</strong> Value </li>
-            <li><strong>Item:</strong> Value </li>
-            <li><strong>Item:</strong> Value </li>
-            <li><strong>Item:</strong> Value </li>
+            <li><strong>Item:</strong> Value</li>
+            <li><strong>Item:</strong> Value</li>
+            <li><strong>Item:</strong> Value</li>
+            <li><strong>Item:</strong> Value</li>
+            <li><strong>Item:</strong> Value</li>
+            <li><strong>Item:</strong> Value</li>
+            <li><strong>Item:</strong> Value</li>
+            <li><strong>Item:</strong> Value</li>
+            <li><strong>Item:</strong> Value</li>
+            <li><strong>Item:</strong> Value</li>
           </ul>
         </div>
 
@@ -143,9 +142,13 @@
             </thead>
             <tbody>
               <!-- just make 20 example reefs -->
-              <tr v-for="i in 20" :key="i" :style="{
-                backgroundColor: i === 7 ? '#CCC' : ''
-              }">
+              <tr
+                v-for="i in 20"
+                :key="i"
+                :style="{
+                  backgroundColor: i === 7 ? '#CCC' : '',
+                }"
+              >
                 <td>Reef {{ i }}</td>
                 <td>Location {{ i }}</td>
                 <td>Country {{ i }}</td>
@@ -161,9 +164,10 @@
       <div id="section-7" class="section">
         <h2>Call to action</h2>
         <div class="measure-wide">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos saepe ducimus natus accusamus beatae error
-          reprehenderit corrupti! Nulla beatae, sed perferendis nisi labore perspiciatis. Suscipit sit maiores placeat
-          error libero.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos saepe
+          ducimus natus accusamus beatae error reprehenderit corrupti! Nulla
+          beatae, sed perferendis nisi labore perspiciatis. Suscipit sit maiores
+          placeat error libero.
         </div>
 
         <!-- email sign up form -->
@@ -179,37 +183,39 @@
           <div class="w-third fl">
             <h2 class="f1">Gov</h2>
             <div>arrow</div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sequi iusto at perferendis quaerat
-              explicabo ut. Quod ducimus doloremque odio, et maxime officia odit, magnam ea pariatur debitis deserunt
-              commodi?</div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+              sequi iusto at perferendis quaerat explicabo ut. Quod ducimus
+              doloremque odio, et maxime officia odit, magnam ea pariatur
+              debitis deserunt commodi?
+            </div>
             <button>CTA</button>
           </div>
           <div class="w-third fl">
             <h2 class="f1">Funders</h2>
             <div>arrow</div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sequi iusto at perferendis quaerat
-              explicabo ut. Quod ducimus doloremque odio, et maxime officia odit, magnam ea pariatur debitis deserunt
-              commodi?</div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+              sequi iusto at perferendis quaerat explicabo ut. Quod ducimus
+              doloremque odio, et maxime officia odit, magnam ea pariatur
+              debitis deserunt commodi?
+            </div>
             <button>CTA</button>
           </div>
           <div class="w-third fl">
             <h2 class="f1">Scientists</h2>
             <div>arrow</div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sequi iusto at perferendis quaerat
-              explicabo ut. Quod ducimus doloremque odio, et maxime officia odit, magnam ea pariatur debitis deserunt
-              commodi?</div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+              sequi iusto at perferendis quaerat explicabo ut. Quod ducimus
+              doloremque odio, et maxime officia odit, magnam ea pariatur
+              debitis deserunt commodi?
+            </div>
             <button>CTA</button>
           </div>
-
         </div>
-
       </div>
     </div>
-
-
-
   </div>
 </template>
-<script setup>
-
-</script>
+<script setup></script>

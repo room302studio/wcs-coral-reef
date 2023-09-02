0c560dc8fa90dd65d54d91cc2a7aebfb7beda514
Curran <curran.kelleher@gmail.com>
2023-09-02 12:51:42 -0400

    the globe is spinning

--- components/SpinnyGlobe.vue
+++ components/SpinnyGlobe.vue
@@ -4,9 +4,11 @@
 <script setup>
 import { select } from "d3-selection";
 import { geoOrthographic, geoPath, geoGraticule } from "d3-geo";
+import { drag } from "d3-drag";
 
 const width = 960;
 const height = 500;
+const sensitivity = 58;
 
 const globeSVG = ref(null);
 
@@ -22,13 +24,22 @@ onMounted(() => {
   const path = geoPath().projection(projection);
 
   const graticule = geoGraticule();
-
-  svg
+  const graticulePath = svg
     .append("path")
     .datum(graticule)
     .attr("class", "graticule")
     .attr("d", path)
     .attr("stroke", "#bbb")
     .attr("fill", "none");
+  const render = () => graticulePath.attr("d", path);
+  svg.call(
+    drag().on("drag", (event) => {
+      const rotate = projection.rotate();
+      const scale = projection.scale();
+      const k = sensitivity / scale;
+      projection.rotate([rotate[0] + event.dx * k, rotate[1] - event.dy * k]);
+      render();
+    })
+  );
 });
 </script>

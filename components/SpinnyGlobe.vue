c116d8133b4c5f1ef8f996aa97e297d9060855f8
Curran <curran.kelleher@gmail.com>
2023-09-05 16:46:34 -0400

    Livestream progress

--- components/SpinnyGlobe.vue
+++ components/SpinnyGlobe.vue
@@ -6,4 +6,17 @@ const stateRef = ref({});
 function setState(next) {
   stateRef.value = next(stateRef.value);
 }
+// state.selectedBCUID
+
+const emit = defineEmits(["update:bcu"]);
+
+watch(
+  () => {
+    return stateRef.value.selectedBCUID;
+  },
+  (newState) => {
+    console.log("state changed", newState);
+    emit("update:bcu", newState);
+  }
+);
 </script>
--- components/globe.js
+++ components/globe.js
@@ -1,7 +1,10 @@
 import { select } from "d3-selection";
 import { geoOrthographic, geoPath, geoGraticule } from "d3-geo";
+import { scaleOrdinal } from "d3-scale";
+import { schemeCategory10 } from "d3-scale-chromatic";
 import { drag } from "d3-drag";
 import { zoom } from "d3-zoom";
+import { feature } from "topojson-client";
 
 // This module implements a spinny globe.
 // It is a component that renders into a container element.
@@ -14,6 +17,9 @@ const path = geoPath().projection(projection);
 const graticule = geoGraticule();
 const dragHandler = drag();
 const zoomHandler = zoom();
+
+const worldAtlasURL =
+  "https://unpkg.com/visionscarto-world-atlas@0.1.0/world/110m.json";
 
 // Sensitivity of panning and zooming
 const sensitivity = 58;
@@ -45,19 +51,114 @@ export const globe = (container, { state, setState }) => {
     return;
   }
 
+  // If the data is not loaded for public/50Reefs_data_extract/50Reefs_good_compromise_BCUs.json
+  // then load it.
+  if (!state.goodCompromiseBCUsData) {
+    fetch("/50Reefs_data_extract/50Reefs_good_compromise_BCUs.json")
+      .then((response) => response.json())
+      .then((topojsonData) => {
+        // console.log(goodCompromiseBCUsData);
+        // console.log(feature(goodCompromiseBCUsData));
+
+        // console.log("feature", feature);
+
+        const goodCompromiseBCUsData = feature(
+          topojsonData,
+          topojsonData.objects["50Reefs_good_compromise_BCUs"]
+        );
+        // console.log("goodCompromiseBCUsData", goodCompromiseBCUsData);
+
+        setState((state) => ({
+          ...state,
+          goodCompromiseBCUsData,
+        }));
+      });
+    return;
+  }
+
+  // Load in the world atlas data
+  if (!state.worldAtlasData) {
+    fetch(worldAtlasURL)
+      .then((response) => response.json())
+      .then((topojsonData) => {
+        // console.log("topojsonData", topojsonData);
+        const worldAtlasData = feature(
+          topojsonData,
+          topojsonData.objects.countries
+        );
+        // console.log("worldAtlasData", worldAtlasData);
+
+        setState((state) => ({
+          ...state,
+          worldAtlasData,
+        }));
+      });
+    return;
+  }
+
   // Update projection rotation based on state
   projection.rotate(state.rotate);
   projection.scale(state.scale);
 
   // Render graticules (lines around the globe)
+  // svg
+  //   .selectAll("path.graticule")
+  //   .data([null])
+  //   .join("path")
+  //   .attr("class", "graticule")
+  //   .attr("d", path(graticule()))
+  //   .attr("stroke", "red")
+  //   .attr("fill", "none");
+
+  // Render shapes from state.worldAtlasData
   svg
-    .selectAll("path.graticule")
-    .data([null])
+    .selectAll("path.worldAtlas")
+    .data(state.worldAtlasData.features)
     .join("path")
-    .attr("class", "graticule")
-    .attr("d", path(graticule()))
-    .attr("stroke", "red")
-    .attr("fill", "none");
+    .attr("class", "worldAtlas")
+    .attr("d", path)
+    // .attr("stroke", "black")
+    .attr("fill", "#ffedd9");
+
+  const colorScale = scaleOrdinal(schemeCategory10);
+
+  console.log(state.goodCompromiseBCUsData.features);
+  // Render shapes from state.goodCompromiseBCUsData
+  svg
+    .selectAll("path.goodCompromiseBCUs")
+    .data(state.goodCompromiseBCUsData.features)
+    .join("path")
+    .attr("class", "goodCompromiseBCUs")
+    .attr("d", path)
+    // .attr("stroke", "black")
+    .attr("fill", (d) => colorScale(d.properties.BCUID))
+    .on("click", (_, d) => {
+      console.log(d.properties.BCUID);
+      setState((state) => ({
+        ...state,
+        selectedBCUID: d.properties.BCUID,
+      }));
+    });
+
+  /* 
+        <path
+      v-for="county in features"
+      :key="'county'+county.id"
+      :class="['county-boundary']"
+      :d="projectedGeoPath(county)"
+      :transform-origin="`${projectedGeoPath.centroid(county)[0]}px ${
+        projectedGeoPath.centroid(county)[1]
+      }px`"
+      :id="county.id"
+      :fill="countyColorByCountyFips.get(county.id)"
+      :stroke-width="strokeWidth"
+      stroke="black"
+      @click="onFeatureClick(county, 'county', county.id)"
+    />
+
+    */
+
+  console.log("scale", state.scale);
 
   // Support panning
   svg.call(

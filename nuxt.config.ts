2fb318791f1d5b2fac4d61e5ea45452c195043fd
EJ Fox <ejfox@ejfox.com>
2023-09-18 22:38:12 -0400

    build: remove meta tag from nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,16 @@ import pkg from './package.json'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      title: pkg.name,
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: pkg.version },
-      ],
-    },
-  },
+  // app: {
+  //   head: {
+  //     title: pkg.name,
+  //     meta: [
+  //       { charset: 'utf-8' },
+  //       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  //       { name: 'description', content: pkg.version },
+  //     ],
+  //   },
+  // },
   ssr: false, // for netlify deploy
   // devtools: { enabled: true },
   css: [
